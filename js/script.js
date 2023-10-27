2b33f3ecbe9f91eb8b745fbfd6b1dd0cf0431811
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2023-10-27 11:04:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function what(){
+    var button=document.getElementById('button3');
+    var heade=document.getElementById('header');
+    var motive=document.getElementById("motive");
+
+    if(heade.style.display=='block'){
+heade.style.display='none';
+motive.style.dispaly= "block";
+        
+    }
+    else{
+heade.style.display='block';
+motive.style.dispaly= "block";
+
+
+    }
+    
+}
